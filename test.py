53a6fa186f0ea967a5440e290508e1b46d985fc1
Johannes Englisch <englisch@shh.mpg.de>
2021-07-20 13:34:40 +0200

    removed obsolete test

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ def test_valid(cldf_dataset, cldf_logger):
 
 def test_forms(cldf_dataset):
     assert len(list(cldf_dataset["FormTable"])) == 50095
-    assert any(f["Form"] == "sɛ̃⁵⁵tsɿ³³tɯ²¹" for f in cldf_dataset["FormTable"])
 
 
 def test_parameters(cldf_dataset):
